84a25c4772658b8fd3334468bcd3852586d42f1d
Andy Mikulski <andy.mikulski@gmail.com>
2017-04-27 18:07:47 -0600

    fix camera etc

---
+++ Assets/ChasePlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasePlayer : MonoBehaviour {
+	public Transform target;
+	private NavMeshAgent agent;
+
+	void Start () {
+		agent = gameObject.AddComponent<NavMeshAgent> ();
+
+		agent.speed = 2f;
+		agent.stoppingDistance = 5f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		agent.SetDestination (target.position);
+	}
+}
